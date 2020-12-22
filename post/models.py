a715ad70af6d1b6555d1d578ea8a8579908a1bfd
Abdelkader belhadj <gaddour04@gmail.com>
2020-12-22 13:21:09 +0100

    create POST

---
+++ post/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+	title=models.CharField(max_length=50)
+	content=models.TextField(max_length=1000)
+	created_at=models.DateTimeField(default=timezone.now)
+	image=models.ImageField(upload_to='post-img/')
