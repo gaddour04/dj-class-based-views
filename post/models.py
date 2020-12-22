0bcde6174afa44bea3df2f36344f08c369c301ad
Abdelkader belhadj <gaddour04@gmail.com>
2020-12-22 14:08:05 +0100

    basic view structure

--- post/models.py
+++ post/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
 	title=models.CharField(max_length=50)
 	content=models.TextField(max_length=1000)
 	created_at=models.DateTimeField(default=timezone.now)
-	image=models.ImageField(upload_to='post-img/')
+	image=models.ImageField()
